22e7b864dac29e4e680cb7f0276f6752483cc8e8
surbhi_rathore_uri_edu <surbhi_rathore_uri_edu@gypsum-gpu002.unity.rc.umass.edu>
2025-03-28 00:20:41 +0000

    Revert 'pushing modified adult and german benchmarks'
    This reverts commit 5d716438a51201c1f6679c75c69cee3fff7e1264

--- fairnessBench/benchmarks/adult/scripts/eval.py
+++ fairnessBench/benchmarks/adult/scripts/eval.py
@@ -6,7 +6,8 @@ import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from aif360.sklearn.metrics import statistical_parity_difference, disparate_impact_ratio, equal_opportunity_difference
 
-# SR: the submission.csv we are using here contains y_test, y_pred and protected_attribute columns and we'll use these to calculate any fair/ performance metric
+# the df we are usinf here contains y test, y pred and a protected attribute
+
 
 def get_score(submission_folder = "../env"): # needs to be fixed as per location of submission.csv file 
     submission_path = os.path.join(submission_folder, "submission.csv")
@@ -16,13 +17,25 @@ def get_score(submission_folder = "../env"): # needs to be fixed as per location
     pred_y = submission['Predicted_y']
     prot_attr = submission['sex']
     
-    # SR: Calculate performance metrics
+    # Calculate performance metrics
     accuracy = accuracy_score(test_y, pred_y)
+    precision = precision_score(test_y, pred_y)
+    recall = recall_score(test_y, pred_y)
+    f1 = f1_score(test_y, pred_y)
     
-    # SR: Calculate fairness metrics
-    disparate_impact = disparate_impact_ratio(y_true=test_y, y_pred=pred_y, prot_attr=prot_attr, priv_group=1)
+    # Calculate fairness metrics
+    sp = statistical_parity_difference(y_true=test_y, y_pred=pred_y, prot_attr=prot_attr, priv_group=1)
+    di = disparate_impact_ratio(y_true=test_y, y_pred=pred_y, prot_attr=prot_attr, priv_group=1)
+    eod = equal_opportunity_difference(y_true = test_y, y_pred = pred_y, prot_attr = prot_attr, priv_group=1)
     
-    performace_df = pd.DataFrame({'acc': [accuracy], 'disparate_impact': [disparate_impact]})
+    performace_df = pd.DataFrame({
+        'acc': [accuracy],
+        'precision': [precision],
+        'recall': [recall],
+        'f1': [f1],
+        'statistical_parity': [sp],
+        'disparate_impact': [di],
+        'equal_opportunity_difference':[eod]})
     
     return performace_df
 
