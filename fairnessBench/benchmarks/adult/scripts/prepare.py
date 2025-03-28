ac0273d0d64fcab9171bab545560f22001d53bcb
surbhi_rathore_uri_edu <surbhi_rathore_uri_edu@gypsum-gpu002.unity.rc.umass.edu>
2025-03-28 00:20:41 +0000

    Revert 'pushing modified adult and german benchmarks'
    This reverts commit 5d716438a51201c1f6679c75c69cee3fff7e1264

--- fairnessBench/benchmarks/adult/scripts/prepare.py
+++ fairnessBench/benchmarks/adult/scripts/prepare.py
@@ -1,7 +1,8 @@
 # prepared by user when setting up
 
 import os
-import pandas as pd 
+# import random
+import pandas as pd # Works on unity not on WSL
 from ucimlrepo import fetch_ucirepo
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.linear_model import LogisticRegression
@@ -9,21 +10,29 @@ from sklearn.model_selection import train_test_split
 
 # pip install ucimlrepo, install this before running
 # used this link to get data : https://archive.ics.uci.edu/dataset/2/adult
+
+
 adult = fetch_ucirepo(id=2) 
 
 # Include code here that  the data from the prepared files
 X = adult.data.features.copy()
 y = adult.data.targets.copy()
 
+
 y["income"] = y["income"].str.rstrip('.')
 y["income"] = y["income"].apply(lambda i: 1 if i == '>50K' else 0)
 X["sex"] = X["sex"].apply(lambda i: 1 if i == 'Male' else 0)
 
-# SR: categorical columns in the data 
+# Encode categorical features 
+# read paper and figure out ifone hot encoding is required here or not
+
+
+
 categorical_columns = ["workclass", "education", "marital-status", "occupation", "relationship", 
                        "race", "native-country"]
 
-# SR: encoding categorical values to numerical values
+
+         
 def categorical_feature_encoder(df, features):
     # AS: This function takes the values of each column and changes the object values to float then to int
     """
@@ -43,20 +52,47 @@ def categorical_feature_encoder(df, features):
     """
     enc = {}
     for cur_f in features:
+        # #Debug
+        # print(df[cur_f])
+
         encoder = OrdinalEncoder()
         df[cur_f] = encoder.fit_transform(df[[cur_f]]).astype(int)
+        # #Debug
+        # print(df[cur_f])
+        # exit()
         enc[cur_f] = encoder
     return df, enc
 
 X, enc = categorical_feature_encoder(X, categorical_columns)
 
-# SR: Splitting to test and train 
+y = y.values.ravel() # flattening because it is in 2D format
+
+y_df = pd.DataFrame(y)
+
+# Split and transforming 
 train_X, test_X, train_y, test_y = train_test_split(X, y_df, test_size=0.2, random_state=42)
 
-os.chdir("../env/")
+# #DEbug
+# print(type(X))
+# print(type(y))
+# print(X.shape)
+# print(y.shape)
+# print()
+# print()
+# print(train_X)
+# print()
+# print()
+# print(train_y)
+# print()
+# print()
+# print(test_X)
+# print()
+# print()
+# print(test_y)
 
-# SR: saving test and train files to read in train.py script
-train_X.to_csv("train_X.csv")
-train_y.to_csv("train_y.csv")
-test_X.to_csv("test_X.csv")
-test_y.to_csv("test_y.csv")
+
+os.chdir("../env/")
+train_X.to_csv("train_X.csv", index=True)
+train_y.to_csv("train_y.csv", index=True)
+test_X.to_csv("test_X.csv", index=True)
+test_y.to_csv("test_y.csv", index=True)
