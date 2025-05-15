a57f124e370674907fd25d4bf498fc24f758933c
AymanBx <ayman_sandouk@uri.edu>
2025-05-15 06:13:41 +0000

    Attempting to print env_run_only_files

--- fairnessBench/runner.py
+++ fairnessBench/runner.py
@@ -29,7 +29,8 @@ def run(agent_cls, args):
         print("Research problem: ", research_problem)
         print("Lower level actions enabled: ", [action.name for action in env.low_level_actions])
         print("High level actions enabled: ", [action.name for action in env.high_level_actions])
-        print("Read only files: ", env.read_only_files, file=sys.stderr) #AS: ??
+        print("Read only files: ", env.read_only_files, file=sys.stderr) 
+        print("Env read only files: ", env.env_read_only_files, file=sys.stderr) 
         print("=====================================")  
 
         # AS: Create agent object from whichever agent was requested in agrs 
