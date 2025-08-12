595c1a417cad5b1171a5fe8a9ca287a12e83c994
AymanBx <ayman_sandouk@uri.edu>
2025-08-12 18:09:19 +0000

    eval intermediate doesn't eval train.py. LLM eval and log eval check for eval model is not None

--- fairnessBench/llm_eval_log.py
+++ fairnessBench/llm_eval_log.py
@@ -172,9 +172,9 @@ def repeat_llm_eval_log(n=5, file_path="main_log", eval_model="granite"):
     """
     Run `llm_eval` multiple times and return a list of results.
     """
+    print(f"Running llm log evaluation...")
     results = []
     for i in range(n):
-        print(f"Running evaluation {i + 1}...")
         result = llm_eval_log(file_path=file_path, eval_model=eval_model)
         results.append(result)
     return results
